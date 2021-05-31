d366972739b8b5c67d862e5f152edca2fcb8b71a
V-MalM <81383838+V-MalM@users.noreply.github.com>
2021-05-31 16:54:59 -0500

    saving changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,111 +9,156 @@ import csv
 #  imports a module datetime to work with dates as date objects.
 from datetime import datetime
 
+
+#*******************************************FUNCTIONS*******************************************
+
+# Funtion that accepts csv sourcefile, reads data and writes to a list, returns it for further analysis
+def readFromCsv(srcFile):
+    # local variable for list into which the source data will be read in to a list of tuples.
+    ProfitLossList = []   
+    with open(srcFile,'r', encoding="utf8") as csvsourcefile:
+         # Reads the data from csv file and creates a dictonary preserving the header with each header filed as a key
+        csvsourcedata = csv.DictReader(csvsourcefile,delimiter=",")
+        for row in csvsourcedata:
+        
+            #coverting Date column that is in str format to datetime so we can sort the data by date  
+            datecol = datetime.strptime(row['Date'],'%b-%Y')         
+            
+            #adding data in to the list as tuples each tuple containing date and corresponding profilt/loss value
+            ProfitLossList.append((datecol, float(row['Profit/Losses']))) 
+    return(ProfitLossList)        
+
+# Function the accepts ProfitLossList (list of tuples) for further analysis and output 
+def analyzeAndWrite(Profit_Loss_List):
+    # sort() method sorts and rearranges the list of tuples in order of date
+    # using lamda function here
+    Profit_Loss_List.sort(key = lambda x:x[0])  
+    # The list is now ready for further calculations
+
+    # 1. The total number of months included in the dataset
+    total_number_of_months  = len(Profit_Loss_List)
+
+    # This calculates rate of change for each month and saves it into a list of tuples with date and change in profit/loss. Inside the loop it also creates list just to store changes in profit/loss. This is to get min and max without having to use list comprehensions that will run a for loop each time an agggregate function is called
+
+    monthly_change_list_tbl_with_date = []
+    list_of_monthly_changes  = []
+    prev = 0
+    rec_count = 0
+    total_Profit_Losses = 0
+    total_change_for_all_months = 0
+    average_change = 0
+    for i in Profit_Loss_List:
+        monthly_change = 0    
+        curr_val = i[1]
+
+        # 2. Calculates the net total amount of "Profit/Losses" over the entire period
+        total_Profit_Losses += curr_val
+        if prev != 0:
+            monthly_change = curr_val - prev
+            rec_count += 1
+        monthly_change_list_tbl_with_date.append((i[0], monthly_change)) 
+        list_of_monthly_changes.append(monthly_change)
+
+        # 3.1 Calculates the changes in "Profit/Losses" over the entire period
+        total_change_for_all_months+= monthly_change
+        prev = i[1]
+            
+        
+    # 3.2 Calculate the average of the changes in "Profit/Losses" over the entire period
+    if rec_count > 0:
+        average_change = round(total_change_for_all_months/rec_count,2)
+
+    # 4.1 gets and stores greatest increase to this variable
+    Greatest_Increase_in_Profits  = max(list_of_monthly_changes)
+
+    # 5.1 gets and stores greatest decrease to this variable
+    Greatest_Decrease_in_Profits  = min(list_of_monthly_changes)
+
+        
+    # Formatting floating values for display 
+    total_Profit_Losses = "{:.0f}".format(total_Profit_Losses)
+    Greatest_Increase_in_Profits_formmatted = "{:.0f}".format(Greatest_Increase_in_Profits)
+    Greatest_Decrease_in_Profits_formmatted = "{:.0f}".format(Greatest_Decrease_in_Profits)
+
+    # The out put string for greatest increase (date and amount) over the entire period
+    GIIP_Str = ""
+
+    # The out put string for greatest decrease (date and amount) over the entire period
+    GDIP_Str = ""
+
+    # Loops throgh the monthly_change_list_tbl_with_date . This will make sure that if there is more than one month with exact rate of change values , it displays those as well 
+    # 4.2. fetches the corresponding date for greatest increase, formats it to string for display and adds to output string
+    # 5.2. fetches the corresponding date for greatest decrease, formats it to string for display and adds to output string
+
+    for i in monthly_change_list_tbl_with_date:
+        if i[1] == Greatest_Increase_in_Profits :
+            GIIPDate = i[0]
+            GIIPDate = GIIPDate.strftime('%b-%Y') 
+            GIIP_Str += 'Greatest Increase in Profits: ' + GIIPDate + ' $(' + str(Greatest_Increase_in_Profits_formmatted) +')'+ ('\n')
+
+        if i[1] == Greatest_Decrease_in_Profits :
+            GDIPDate = i[0]
+            GDIPDate = GDIPDate.strftime('%b-%Y')
+            GDIP_Str += 'Greatest Decrease in Losses: ' + GDIPDate + ' $(' + str(Greatest_Decrease_in_Profits_formmatted) +')'+ ('\n')
+
+    # Analysis Output 
+    Output_str = ('\n')
+    Output_str = Output_str + 'Financial Analysis'+ ('\n')
+    Output_str = Output_str + '----------------------------'+ ('\n')
+    Output_str = Output_str + 'Total Months:' + str(total_number_of_months)+ ('\n')
+    Output_str = Output_str + 'Total: $' + str(total_Profit_Losses)+ ('\n')
+    Output_str = Output_str + 'Average  Change: $' +  str(average_change)+ ('\n')
+    Output_str = Output_str + GIIP_Str
+    Output_str = Output_str + GDIP_Str
+    Output_str = Output_str + ('\n')
+
+    # Print the analysis to the terminal
+    print(Output_str)
+
+    # Path to Output file in the 'analysis' folder
+    # 'budget_analysis.txt'
+    output_file = os.path.join(os.getcwd(),'analysis','budget_analysis.txt') 
+
+    # Write results to output file
+    try: 
+        with open (output_file, mode="w", newline='') as txtfile:
+            txtfile.write(Output_str)
+    except FileNotFoundError :  
+        print ("\n********OUTPUT FOLDER NOT FOUND. Results to output file cannot be written********\n")  
+        print ("Output folder not found or the path is incorrect. Please make sure your working \ndirectory is PyBank and it has 'analysis' folder.\n") 
+        print ("**********************************************************************************\n")    
+        exit 
+
+#*******************************************END OF FUNCTIONS*******************************************
+
+
 # Path to collect data from the 'Resources' folder
 # 'budget_data.csv'
 source_file = os.path.join(os.getcwd(),'Resources','budget_data.csv') 
 
 # list into which the source data will be read in to a list of tuples.
-Profit_Loss_List = []                                                   
-with open(source_file,'r', encoding="utf8") as csvsourcefile:
-    # Reads the data from csv file and creates a dictonary preserving the header with each header filed as a key
-    csvsourcedata = csv.DictReader(csvsourcefile,delimiter=",")
-    for row in csvsourcedata:
+PftLssLst = []   
+
+# Error handling if the file is not found or path is incorrect, it will message the user. 
+try:
+    PftLssLst = readFromCsv(source_file)                                            
     
-        #coverting Date column that is in str format to datetime so we can sort the data by date  
-        datecol = datetime.strptime(row['Date'],'%b-%Y')         
-        
-        #adding data in to the list as tuples each tuple containing date and corresponding profilt/loss value
-        Profit_Loss_List.append((datecol, float(row['Profit/Losses']))) 
+# if the file is not found or path is incorrect, it will message the user. 
+except FileNotFoundError :  
+    print ("\n******************************** FILE NOT FOUND ********************************\n")  
+    print ("File not found or the path is incorrect. Please make sure your working directory\nis PyBank and it has both 'Resources' and 'analysis' folders. \nAlso make sure file 'budget_data.csv' is present in 'Resources' folder\n") 
+    print ("*********************************************************************************\n")    
+    exit  
 
-# sort() method sorts and rearranges the list of tuples in order of date
-# using lamda function here
-Profit_Loss_List.sort(key = lambda x:x[0])  
-# The list is now ready for further calculations
+# Any other error 
+except:
+    print ("\n******************************** Unknown Error ********************************\n")  
+    print ("Error Occured. Plase contact the IT department\n") 
+    print ("*********************************************************************************\n")    
+    exit  
 
-# 1. The total number of months included in the dataset
-total_number_of_months  = len(Profit_Loss_List)
+# if no errors, calls the function to analyse and generate output 
+else:
+    # calles function  analyzeAndWrite and passes the PftLssLst returned by previous function
+    analyzeAndWrite(PftLssLst)
 
-# This calculates rate of change for each month and saves it into a list of tuples with date and change in profit/loss. Inside the loop it also creates list just to store changes in profit/loss. This is to get min and max without having to use list comprehensions that will run a for loop each time an agggregate function is called
-
-monthly_change_list_tbl_with_date = []
-list_of_monthly_changes  = []
-prev = 0
-rec_count = 0
-total_Profit_Losses = 0
-total_change_for_all_months = 0
-average_change = 0
-for i in Profit_Loss_List:
-    monthly_change = 0    
-    curr_val = i[1]
-
-    # 2. Calculates the net total amount of "Profit/Losses" over the entire period
-    total_Profit_Losses += curr_val
-    if prev != 0:
-        monthly_change = curr_val - prev
-        rec_count += 1
-    monthly_change_list_tbl_with_date.append((i[0], monthly_change)) 
-    list_of_monthly_changes.append(monthly_change)
-
-    # 3.1 Calculates the changes in "Profit/Losses" over the entire period
-    total_change_for_all_months+= monthly_change
-    prev = i[1]
-        
-    
-# 3.2 Calculate the average of the changes in "Profit/Losses" over the entire period
-average_change = round(total_change_for_all_months/rec_count,2)
-
-# 4.1 gets and stores greatest increase to this variable
-Greatest_Increase_in_Profits  = max(list_of_monthly_changes)
-
-# 5.1 gets and stores greatest decrease to this variable
-Greatest_Decrease_in_Profits  = min(list_of_monthly_changes)
-
-    
-# Formatting floating values for display 
-total_Profit_Losses = "{:.0f}".format(total_Profit_Losses)
-Greatest_Increase_in_Profits_formmatted = "{:.0f}".format(Greatest_Increase_in_Profits)
-Greatest_Decrease_in_Profits_formmatted = "{:.0f}".format(Greatest_Decrease_in_Profits)
-
-# The out put string for greatest increase (date and amount) over the entire period
-GIIP_Str = ""
-
-# The out put string for greatest decrease (date and amount) over the entire period
-GDIP_Str = ""
-
-# Loops throgh the monthly_change_list_tbl_with_date . This will make sure that if there is more than one month with exact rate of change values , it displays those as well 
-# 4.2. fetches the corresponding date for greatest increase, formats it to string for display and adds to output string
-# 5.2. fetches the corresponding date for greatest decrease, formats it to string for display and adds to output string
-
-for i in monthly_change_list_tbl_with_date:
-    if i[1] == Greatest_Increase_in_Profits :
-        GIIPDate = i[0]
-        GIIPDate = GIIPDate.strftime('%b-%Y') 
-        GIIP_Str += 'Greatest Increase in Profits: ' + GIIPDate + ' $(' + str(Greatest_Increase_in_Profits_formmatted) +')'+ ('\n')
-
-    if i[1] == Greatest_Decrease_in_Profits :
-        GDIPDate = i[0]
-        GDIPDate = GDIPDate.strftime('%b-%Y')
-        GDIP_Str += 'Greatest Decrease in Losses: ' + GDIPDate + ' $(' + str(Greatest_Decrease_in_Profits_formmatted) +')'+ ('\n')
-
-# Analysis Output 
-Output_str = ('\n')
-Output_str = Output_str + 'Financial Analysis'+ ('\n')
-Output_str = Output_str + '----------------------------'+ ('\n')
-Output_str = Output_str + 'Total Months:' + str(total_number_of_months)+ ('\n')
-Output_str = Output_str + 'Total: $' + str(total_Profit_Losses)+ ('\n')
-Output_str = Output_str + 'Average  Change: $' +  str(average_change)+ ('\n')
-Output_str = Output_str + GIIP_Str
-Output_str = Output_str + GDIP_Str
-Output_str = Output_str + ('\n')
-
-# Print the analysis to the terminal
-print(Output_str)
-
-# Path to Output file in the 'analysis' folder
-# 'budget_analysis.txt'
-output_file = os.path.join(os.getcwd(),'analysis','budget_analysis.txt') 
-
-# Write results to output file
-with open (output_file, mode="w", newline='') as txtfile:
-    txtfile.write(Output_str)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,6 +6,84 @@
 import os
 import csv
 
+#*******************************************FUNCTIONS*******************************************
+
+# Funtion that accepts csv sourcefile, reads data and writes to a list and a set , returns them for further analysis
+def readFromCsv(srcFile):
+    candidateVotes = [] # 2. A complete list of candidates who received votes
+    candidateListUnique = set() # To create a list of unique candidates
+    with open(srcFile,'r', encoding="utf8") as csvsourcefile:
+
+        # Reads the data from csv file and creates a dictonary preserving the header with each header filed as a key
+        csvsourcedata = csv.DictReader(csvsourcefile,delimiter=",")
+        for row in csvsourcedata:
+            candidateVotes.append(row['Candidate'])
+            candidateListUnique.add(row['Candidate'])  
+
+    return  candidateVotes, candidateListUnique     
+
+# Function the accepts candidatevotes list candidateListUnique set for further analysis and output 
+def analyzeAndWrite(candVotes, candUniqueList):
+    candidate_vote_count_dict = {} #dictionary to store each candidate name and their total vote count. This can be done using tuples aswell. but i am using a dictonary because I used tuples in PyBank
+
+    for i in candUniqueList :
+        candidate_vote_count_dict[i] = candVotes.count(i)
+
+    # sorting the dictonary in descending order of candidate's number of votes. Dictonary doesn't have a provision to be sorted but dictonary has items() method that returns a view of list of tuples, each tuple having key and value of each pair in a dictionary, that can be sorted. I used sorted() function here to sort.
+
+    d_sorted = sorted(candidate_vote_count_dict.items(),key=lambda x:x[1],reverse=True)
+
+    # 1. The total number of votes cast
+    Total_Votes = len(candidate_votes)
+
+    res_Output_str = ""
+    per_votes = 0
+
+    # looping through the sorted list to display candidates and their votes starting with the candidate with highest number of votes
+    for i in d_sorted:
+
+        # Calculating percentage of votes for each candidate
+        per_votes = (i[1]/Total_Votes)*100
+
+        # 3.1 Creating an output string that shows candidate name and the percentage of votes each candidate won
+        # 3.2 The out put string also has the total number of votes each candidate won
+        res_Output_str += i[0] + ": " + "{:.3f}".format(per_votes) + "% (" + str(i[1]) + ')\n'
+
+    # The winner of the election based on popular vote. it is the first value of the first tuple from sorted list 
+    winner_str = 'Winner: ' +  d_sorted[0][0] +'\n'
+
+    # Result Output 
+    Output_str = ('\n')
+    Output_str += 'Election Results'+'\n'
+    Output_str += '-------------------------' +'\n'
+    Output_str += 'Total Votes: ' + str(Total_Votes)+'\n'
+    Output_str += '-------------------------' +'\n'
+    Output_str += res_Output_str
+    Output_str += '-------------------------' + '\n'
+    Output_str += winner_str
+    Output_str += '-------------------------' +'\n'
+    Output_str += ('\n')
+
+    # Print the election result to the terminal
+    print(Output_str)
+
+    # Path to Output file in the 'analysis' folder
+    # 'election_results.txt'
+    output_file = os.path.join(os.getcwd(),'analysis','election_results.txt') 
+
+    # Write results to output file
+    try: 
+        with open (output_file, mode="w") as txtfile:
+            txtfile.write(Output_str)
+    except FileNotFoundError :  
+        print ("\n********OUTPUT FOLDER NOT FOUND. Results to output file cannot be written********\n")  
+        print ("Output folder not found or the path is incorrect. Please make sure your working \ndirectory is PyPoll and it has 'analysis' folder.\n") 
+        print ("**********************************************************************************\n")    
+        exit       
+
+#*******************************************END OF FUNCTIONS*******************************************
+
+
 # Path to collect data from the 'Resources' folder
 # 'election_data.csv'
 source_file = os.path.join(os.getcwd(),'Resources','election_data.csv')
@@ -13,61 +91,28 @@ source_file = os.path.join(os.getcwd(),'Resources','election_data.csv')
 candidate_votes = [] # 2. A complete list of candidates who received votes
 candidate_list_unique = set() # To create a list of unique candidates
 
-with open(source_file,'r', encoding="utf8") as csvsourcefile:
+# Error handling if the file is not found or path is incorrect, it will message the user. 
+try:
+    # calles function  readFromCsv and passes the source_file
+    candidate_votes, candidate_list_unique = readFromCsv(source_file)
 
-    # Reads the data from csv file and creates a dictonary preserving the header with each header filed as a key
-    csvsourcedata = csv.DictReader(csvsourcefile,delimiter=",")
-    for row in csvsourcedata:
-        candidate_votes.append(row['Candidate'])
-        candidate_list_unique.add(row['Candidate'])  
+# if the file is not found or path is incorrect, it will message the user. 
+except FileNotFoundError :  
+    print ("\n******************************** FILE NOT FOUND ********************************\n")  
+    print ("File not found or the path is incorrect. Please make sure your working directory\nis PyPoll and it has both 'Resources' and 'analysis' folders. \nAlso make sure file 'election_data.csv' is present in 'Resources' folder\n") 
+    print ("*********************************************************************************\n")    
+    exit   
 
-candidate_vote_count_dict = {} #dictionary to store each candidate name and their total vote count. This can be done using tuples aswell. but i am using a dictonary because I used tuples in PyBank
+# Any other error 
+except:
+    print ("\n******************************** Unknown Error ********************************\n")  
+    print ("Error Occured. Plase contact the IT department\n") 
+    print ("*********************************************************************************\n")    
+    exit  
 
-for i in candidate_list_unique :
-    candidate_vote_count_dict[i] = candidate_votes.count(i)
+# if no errors, calls the function to analyse and generate output    
+else:
+    # calles function  analyzeAndWrite and passes the candidate_votes, candidate_list_unique returned by previous function
+    analyzeAndWrite(candidate_votes, candidate_list_unique)
 
-# sorting the dictonary in descending order of candidate's number of votes. Dictonary doesn't have a provision to be sorted but dictonary has items() method that returns a view of list of tuples, each tuple having key and value of each pair in a dictionary, that can be sorted. 
-
-d_sorted = sorted(candidate_vote_count_dict.items(),key=lambda x:x[1],reverse=True)
-
-# 1. The total number of votes cast
-Total_Votes = len(candidate_votes)
-
-res_Output_str = ""
-per_votes = 0
-
-# looping through the sorted list to display candidates and their votes starting with the candidate with highest number of votes
-for i in d_sorted:
-
-    # Calculating percentage of votes for each candidate
-    per_votes = (i[1]/Total_Votes)*100
-
-    # 3.1 Creating an output string that shows candidate name and the percentage of votes each candidate won
-    # 3.2 The out put string also has the total number of votes each candidate won
-    res_Output_str += i[0] + ": " + "{:.3f}".format(per_votes) + "% (" + str(i[1]) + ')\n'
-
-# The winner of the election based on popular vote. it is the first value of the first tuple from sorted list 
-winner_str = 'Winner: ' +  d_sorted[0][0] +'\n'
-
-# Result Output 
-Output_str = ('\n')
-Output_str += 'Election Results'+'\n'
-Output_str += '-------------------------' +'\n'
-Output_str += 'Total Votes: ' + str(Total_Votes)+'\n'
-Output_str += '-------------------------' +'\n'
-Output_str += res_Output_str
-Output_str += '-------------------------' + '\n'
-Output_str += winner_str
-Output_str += '-------------------------' +'\n'
-Output_str += ('\n')
-
-# Print the election result to the terminal
-print(Output_str)
-
-# Path to Output file in the 'analysis' folder
-# 'election_results.txt'
-output_file = os.path.join(os.getcwd(),'analysis','election_results.txt') 
-
-# Write results to output file
-with open (output_file, mode="w") as txtfile:
-    txtfile.write(Output_str)
+      
